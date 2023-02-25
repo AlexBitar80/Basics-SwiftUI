aed28459480e46bd206d05a078212d0900ebbfba
joão Alexandre Bitar <joaoalexbitar@brq.com>
2023-02-25 14:17:44 -0300

    Initial Commit

---
+++ BasicsSwiftUI/BasicsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicsSwiftUIApp.swift
+//  BasicsSwiftUI
+//
+//  Created by João Alexandre Bitar on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
